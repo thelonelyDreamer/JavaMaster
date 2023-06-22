e70589a6405603bf9edd381decd1d105cbbf6374
felix wang <2316700204@qq.com>
2023-06-22 16:30:43 +0800

    ok

--- java8/java8-nio/src/main/java/org/example/Main.java
+++ java8/java8-nio/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.nio.ByteBuffer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ByteBuffer buffer = ByteBuffer.allocate(8);
+        buffer.put("12345678".getBytes());
+        buffer.flip();
+        System.out.println(new String(buffer.array()));
     }
 }
